6b5255ba6d556eec23c2626117f99f3e82e8bd9f
Sawyereva <106629658+Sawyereva@users.noreply.github.com>
2024-04-10 23:41:40 -0600

    Admin edit and delete

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BrickedUpBrickBuyer.Data;
 using BrickedUpBrickBuyer.Data.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BrickedUpBrickBuyer.Controllers
 {
@@ -19,13 +21,67 @@ namespace BrickedUpBrickBuyer.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult AddOrder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddOrder(Order neworder)
+        {
+            if (ModelState.IsValid)
+            {
+                _brickRepository.AddOrder(neworder);
+
+                var ordoos = _brickRepository.Orders.ToList();
+                return View("Orders", ordoos);
+            }
+            else
+            {
+                return View(neworder);
+            }
+        }
         public IActionResult EditOrder(int orderid)
         {
             var orderdude = new Order();
             orderdude = _brickRepository.Orders.ToList()
                 .Where(x => x.TransactionId == orderid)
                 .FirstOrDefault();
-            return View(orderdude);
+            return View("AddOrder",orderdude);
+        }
+        public IActionResult DeleteOrder(int orderid)
+        {
+            var record = _brickRepository.Orders
+                .Single(x => x.TransactionId == orderid);
+
+            _brickRepository.RemoveOrder(record);
+
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product newproduct)
+        {
+            if (ModelState.IsValid)
+            {
+                _brickRepository.AddProduct(newproduct);
+
+                var prodies = _brickRepository.Products.ToList();
+                return View("Products", prodies);
+            }
+            else
+            {
+                return View(newproduct);
+            }
         }
         public IActionResult EditProduct(int productid)
         {
@@ -33,7 +89,39 @@ namespace BrickedUpBrickBuyer.Controllers
             productdude = _brickRepository.Products.ToList()
                 .Where(x => x.ProductId == productid)
                 .FirstOrDefault();
-            return View(productdude);
+            return View("AddProduct", productdude);
+        }
+        public IActionResult DeleteProduct(int productid)
+        {
+            var record = _brickRepository.Products
+                .Single(x => x.ProductId == productid);
+
+            _brickRepository.RemoveProduct(record);
+
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(Customer newcustomer)
+        {
+            if (ModelState.IsValid)
+            {
+                _brickRepository.AddUser(newcustomer);
+
+                var custos = _brickRepository.Customers.ToList();
+                return View("Customers", custos);
+            }
+            else
+            {
+                return View(newcustomer);
+            }
         }
         public IActionResult EditUser(int userid)
         {
@@ -41,7 +129,16 @@ namespace BrickedUpBrickBuyer.Controllers
             userdude = _brickRepository.Customers.ToList()
                 .Where(x => x.customer_ID == userid)
                 .FirstOrDefault();
-            return View(userdude);
+            return View("AddUser", userdude);
+        }
+        public IActionResult DeleteUser(int userid)
+        {
+            var record = _brickRepository.Customers
+                .Single(x => x.customer_ID == userid);
+
+            _brickRepository.RemoveUser(record);
+
+            return RedirectToAction("Index");
         }
         public IActionResult Orders()
         {
--- Data/IBrickRepository.cs
+++ Data/IBrickRepository.cs
@@ -8,6 +8,14 @@
         public IQueryable<Order> Orders { get; }
         public IQueryable<Product> Products { get; }
 
-
-	}
+        public void AddOrder(Order order);
+        public void RemoveOrder(Order delete);
+        public void UpdateOrder(Order update);
+        public void AddProduct(Product product);
+        public void RemoveProduct(Product delete);
+        public void UpdateProduct(Product update);
+        public void AddUser(Customer customer);
+        public void RemoveUser(Customer delete);
+        public void UpdateUser(Customer update);
+    }
 }
